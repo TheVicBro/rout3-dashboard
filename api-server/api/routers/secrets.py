45371597c2758016229813b15fdb276bf9357fb6
Alain Ballen <alainballen@gmail.com>
2024-06-30 15:46:29 -0400

    jwt and oauth2

--- api-server/api/routers/secrets.py
+++ api-server/api/routers/secrets.py
@@ -4,36 +4,34 @@ from models.schemas import schemas
 from db.database import get_db
 from db.repositories import secrets_repository as secrets_repo
 from db.repositories import user_repository as user_repo
-from services.authentication import get_current_user
-
+from services.jwt import get_current_user
+from typing_extensions import Annotated
+from models.models import User
+from typing import List
 
 router = APIRouter(prefix="/secrets")
 
 
-@router.post("/create")
+@router.post("/create", response_model=schemas.Secret)
 def create_key(
+    current_user: Annotated[str, Depends(get_current_user)],
     secret: schemas.SecretCreate,
     db: Session = Depends(get_db),
-    current_user: Session = Depends(get_current_user),
 ):
     current_user_id = user_repo.get_user_by_username(db, current_user).id
     return secrets_repo.create_secret(db=db, secret=secret, user_id=current_user_id)
 
 
-@router.get("/list")
+@router.get("/list", response_model=List[schemas.Secret])
 def read_current_user_secrets(
-    current_user: Session = Depends(get_current_user),
     skip: int = 0,
     limit: int = 10,
     db: Session = Depends(get_db),
 ):
-    current_user_id = user_repo.get_user_by_username(db, current_user).id
-    return secrets_repo.get_secrets_by_user_id(
-        db, user_id=current_user_id, skip=skip, limit=limit
-    )
+    return secrets_repo.get_secrets_by_user_id(db, user_id=1, skip=skip, limit=limit)
 
 
-@router.get("/list?user_id={user_id}")
+@router.get("/list", response_model=List[schemas.Secret])
 def read_secrets_by_user_id(
     user_id: int,
     skip: int = 0,
--- api-server/models/models.py
+++ api-server/models/models.py
@@ -11,7 +11,7 @@ class Token(BaseModel):
 
 
 class TokenData(BaseModel):
-    username: Union[str, None] = None
+    username: str | None = None
 
 
 class User(Base):
