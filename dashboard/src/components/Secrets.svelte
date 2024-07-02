436e59f2b3d7dcdfb29d51c6bce21d32452bb7b4
Victor Chung <dragonslayer3211@gmail.com>
2024-07-01 20:53:47 -0400

    Registration and secret key censor

--- dashboard/src/components/Secrets.svelte
+++ dashboard/src/components/Secrets.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import { createQuery, useQueryClient } from '@tanstack/svelte-query';
+  import { createQuery, useQueryClient } from "@tanstack/svelte-query";
 
   const token = localStorage.getItem("authToken");
   const queryClient = useQueryClient();
@@ -13,10 +13,10 @@
   };
 
   const addNewKey = async () => {
-    const response = await fetch('http://127.0.0.1:8000/secrets/create', {
-      method: 'POST',
+    const response = await fetch("http://127.0.0.1:8000/secrets/create", {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
         Authorization: `Basic ${token}`,
       },
       body: JSON.stringify({
@@ -27,15 +27,15 @@
     if (!response.ok) {
       throw new Error(`Network response was not ok: ${response.statusText}`);
     }
-    queryClient.invalidateQueries({ queryKey: ['repoData'] });
+    queryClient.invalidateQueries({ queryKey: ["repoData"] });
     addKeyPopup = false;
-  }
+  };
 
   const removeKey = async () => {
-    const response = await fetch('http://127.0.0.1:8000/secrets/delete', {
-      method: 'DELETE',
+    const response = await fetch("http://127.0.0.1:8000/secrets/delete", {
+      method: "DELETE",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
         Authorization: `Basic ${token}`,
       },
       body: JSON.stringify({
@@ -45,22 +45,22 @@
     if (!response.ok) {
       throw new Error(`Network response was not ok: ${response.statusText}`);
     }
-    queryClient.invalidateQueries({ queryKey: ['repoData'] });
-<<<<<<< HEAD
+    queryClient.invalidateQueries({ queryKey: ["repoData"] });
+
     removeKeyPopup = false;
-=======
-    addKeyPopup = false;
->>>>>>> 3917470 (Begun work on removing key functionality and added icons)
-  }
+  };
 
   const fetchRepos = async (): Promise<Repo[]> => {
-    const response = await fetch('http://127.0.0.1:8000/secrets/list?user_id=1', {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Basic ${token}`,
-      }
-    });
+    const response = await fetch(
+      "http://127.0.0.1:8000/secrets/list?user_id=1",
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Basic ${token}`,
+        },
+      },
+    );
     if (!response.ok) {
       throw new Error(`Network response was not ok: ${response.statusText}`);
     }
@@ -68,29 +68,28 @@
   };
 
   const query = createQuery<Repo[]>({
-    queryKey: ['repoData'],
+    queryKey: ["repoData"],
     queryFn: fetchRepos,
   });
 
   let addKeyPopup = false;
-  let newName = '';
-  let newKey = '';
+  let newName = "";
+  let newKey = "";
 
   let removeKeyPopup = false;
   let selectedSecretId: number | null = null;
-<<<<<<< HEAD
 
   const maskKey = (key: string) => {
-    return key.replace(/./g, '*');
-  }
-=======
->>>>>>> 3917470 (Begun work on removing key functionality and added icons)
+    return key.replace(/./g, "*");
+  };
 </script>
 
 <div>
   <h1 class="p-8 pl-20 text-3xl font-bold bg-white border-b-2">Secrets</h1>
   <div class="m-10 border rounded-lg bg-white shadow">
-    <h2 class="p-10 pb-4 leading-none text-2xl font-semibold border-b-2">Overview</h2>
+    <h2 class="p-10 pb-4 leading-none text-2xl font-semibold border-b-2">
+      Overview
+    </h2>
     <div class="p-10">
       {#if $query.isPending}
         Loading...
@@ -120,27 +119,49 @@
       {/if}
     </div>
   </div>
-  <button class="ml-10 px-8 py-2 bg-blue-800 transition hover:bg-blue-700 hover:transition text-white rounded-lg" on:click={() => addKeyPopup = true}>+ Add a new key</button>
-  <button class="ml-10 px-8 py-2 bg-blue-800 transition hover:bg-blue-700 hover:transition text-white rounded-lg" on:click={() => removeKeyPopup = true}>+ Remove a key</button>
+  <button
+    class="ml-10 px-8 py-2 bg-blue-800 transition hover:bg-blue-700 hover:transition text-white rounded-lg"
+    on:click={() => (addKeyPopup = true)}>+ Add a new key</button
+  >
+  <button
+    class="ml-10 px-8 py-2 bg-blue-800 transition hover:bg-blue-700 hover:transition text-white rounded-lg"
+    on:click={() => (removeKeyPopup = true)}>+ Remove a key</button
+  >
 
   {#if addKeyPopup}
-    <div class="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
+    <div
+      class="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50"
+    >
       <div class="bg-white rounded-lg shadow-lg p-8 w-96 relative">
-        <button class="absolute pb-1 top-4 right-4 text-gray-500 hover:text-gray-700 text-4xl rounded-full h-12 w-12 flex items-center justify-center hover:bg-gray-200 transition duration-200 ease-in-out" on:click={() => addKeyPopup = false}>
+        <button
+          class="absolute pb-1 top-4 right-4 text-gray-500 hover:text-gray-700 text-4xl rounded-full h-12 w-12 flex items-center justify-center hover:bg-gray-200 transition duration-200 ease-in-out"
+          on:click={() => (addKeyPopup = false)}
+        >
           &times;
         </button>
         <h2 class="text-2xl font-semibold mb-4">Add New Key</h2>
         <div class="space-y-4">
           <div>
             <div class="block text-gray-700">Name</div>
-            <input type="text" bind:value={newName} class="form-input mt-1 block w-full border rounded p-2" />
+            <input
+              type="text"
+              bind:value={newName}
+              class="form-input mt-1 block w-full border rounded p-2"
+            />
           </div>
           <div>
             <div class="block text-gray-700">Key</div>
-            <input type="text" bind:value={newKey} class="form-input mt-1 block w-full border rounded p-2" />
+            <input
+              type="text"
+              bind:value={newKey}
+              class="form-input mt-1 block w-full border rounded p-2"
+            />
           </div>
           <div class="pt-6">
-            <button class="px-4 py-2 bg-blue-800 text-white rounded-lg hover:bg-blue-700 focus:outline-none" on:click={addNewKey}>Add Key</button>
+            <button
+              class="px-4 py-2 bg-blue-800 text-white rounded-lg hover:bg-blue-700 focus:outline-none"
+              on:click={addNewKey}>Add Key</button
+            >
           </div>
         </div>
       </div>
@@ -148,9 +169,14 @@
   {/if}
 
   {#if removeKeyPopup}
-    <div class="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
+    <div
+      class="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50"
+    >
       <div class="bg-white rounded-lg shadow-lg p-8 w-96 relative">
-        <button class="absolute pb-1 top-4 right-4 text-gray-500 hover:text-gray-700 text-4xl rounded-full h-12 w-12 flex items-center justify-center hover:bg-gray-200 transition duration-200 ease-in-out" on:click={() => removeKeyPopup = false}>
+        <button
+          class="absolute pb-1 top-4 right-4 text-gray-500 hover:text-gray-700 text-4xl rounded-full h-12 w-12 flex items-center justify-center hover:bg-gray-200 transition duration-200 ease-in-out"
+          on:click={() => (removeKeyPopup = false)}
+        >
           &times;
         </button>
         <h2 class="text-2xl font-semibold mb-4">Remove Key</h2>
@@ -158,7 +184,10 @@
           {#if $query.isSuccess}
             <div>
               <div class="block text-gray-700">Select Secret to Remove</div>
-              <select bind:value={selectedSecretId} class="form-select mt-1 block w-full border rounded p-2">
+              <select
+                bind:value={selectedSecretId}
+                class="form-select mt-1 block w-full border rounded p-2"
+              >
                 <option value="" disabled selected>Select a key</option>
                 {#each $query.data as repo}
                   <option value={repo.id}>{repo.name}</option>
@@ -167,7 +196,10 @@
             </div>
           {/if}
           <div class="pt-6">
-            <button class="px-4 py-2 bg-blue-800 text-white rounded-lg hover:bg-blue-700 focus:outline-none" on:click={removeKey}>Remove Key</button>
+            <button
+              class="px-4 py-2 bg-blue-800 text-white rounded-lg hover:bg-blue-700 focus:outline-none"
+              on:click={removeKey}>Remove Key</button
+            >
           </div>
         </div>
       </div>
