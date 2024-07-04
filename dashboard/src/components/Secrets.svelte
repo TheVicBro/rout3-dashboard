b20b1125c3553e0fa97b42d9a6d932b8db905e86
Victor Chung <dragonslayer3211@gmail.com>
2024-07-04 16:32:18 -0400

    Fixed secret adding and removing

--- dashboard/src/components/Secrets.svelte
+++ dashboard/src/components/Secrets.svelte
@@ -16,8 +16,13 @@
     const response = await fetch("http://127.0.0.1:8000/secrets/create", {
       method: "POST",
       headers: {
+<<<<<<< HEAD
         "Content-Type": "application/json",
         Authorization: `Basic ${token}`,
+=======
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+>>>>>>> bdaeeec (Fixed secret adding and removing)
       },
       body: JSON.stringify({
         name: newName,
@@ -32,15 +37,23 @@
   };
 
   const removeKey = async () => {
+<<<<<<< HEAD
     const response = await fetch("http://127.0.0.1:8000/secrets/delete", {
       method: "DELETE",
       headers: {
         "Content-Type": "application/json",
         Authorization: `Basic ${token}`,
+=======
+    if (selectedSecretId === null) {
+      throw new Error('No secret selected for removal');
+    }
+    const response = await fetch(`http://127.0.0.1:8000/secrets/delete?secret_id=${selectedSecretId}`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+>>>>>>> bdaeeec (Fixed secret adding and removing)
       },
-      body: JSON.stringify({
-        secret_id: selectedSecretId,
-      }),
     });
     if (!response.ok) {
       throw new Error(`Network response was not ok: ${response.statusText}`);
@@ -78,10 +91,13 @@
 
   let removeKeyPopup = false;
   let selectedSecretId: number | null = null;
+<<<<<<< HEAD
 
   const maskKey = (key: string) => {
     return key.replace(/./g, "*");
   };
+=======
+>>>>>>> bdaeeec (Fixed secret adding and removing)
 </script>
 
 <div>
@@ -98,6 +114,9 @@
         An error has occurred: {$query.error.message}
       {/if}
       {#if $query.isSuccess}
+        {#if $query.data.length === 0}
+          <div class="mb-4 text-red-600">No secrets found. Click "Add key" to add a new key.</div>
+        {/if}
         <table class="w-full">
           <thead>
             <tr>
@@ -110,7 +129,7 @@
             {#each $query.data as repo}
               <tr>
                 <td class="p-2">{repo.name}</td>
-                <td class="p-2">{maskKey(repo.key)}</td>
+                <td class="p-2">**********</td>
                 <td class="p-2">{repo.last_used}</td>
               </tr>
             {/each}
