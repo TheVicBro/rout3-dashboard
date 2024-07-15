ed31e3ce3a59de6599b013a4ecd42fa8581da5d9
Dinuja Wattage <dinuja.w9@gmail.com>
2024-07-15 13:09:05 -0400

    updated attribute names in secrets schema

---
+++ api-server/schemas/schemas.py
@@ -0,0 +1,44 @@
+from pydantic import BaseModel
+from typing import List, Optional
+
+
+class SecretBase(BaseModel):
+    provider: str
+    last_used: str
+
+
+class SecretCreate(SecretBase):
+    """
+    Seperate key from base model
+    so it won't be sent from API when reading a secrets
+    """
+    key: str
+
+
+class Secret(SecretBase):
+    id: int
+    user_id: int
+
+    class Config:
+        orm_mode = True
+
+
+class UserBase(BaseModel):
+    username: str
+
+
+class UserCreate(UserBase):
+    """
+    Seperate password from base model
+    so it won't be sent from API when reading a user
+    """
+
+    password: str
+
+
+class User(UserBase):
+    id: int
+    secrets: List[Secret] = []
+
+    class Config:
+        orm_mode = True
