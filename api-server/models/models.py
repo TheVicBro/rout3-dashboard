efcc7fd0be525994e041c16cf8dfe40dd5763797
Dinuja Wattage <dinuja.w9@gmail.com>
2024-07-15 13:09:15 -0400

    added usage table

--- api-server/models/models.py
+++ api-server/models/models.py
@@ -1,5 +1,5 @@
 from typing import Union
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String, Float, ForeignKey
 from sqlalchemy.orm import relationship
 from db.database import Base
 from pydantic import BaseModel
@@ -19,8 +19,8 @@ class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True)
-    username = Column(String, index=True)
-    hashed_password = Column(String)
+    username = Column(String, index=True, nullable=False)
+    hashed_password = Column(String, nullable=False)
     # is_active = Column(Boolean, default=True)
 
     secrets = relationship("Secret", back_populates="user")
@@ -30,9 +30,27 @@ class Secret(Base):
     __tablename__ = "secrets"
 
     id = Column(Integer, primary_key=True)
-    name = Column(String)
-    key = Column(String)
+    provider = Column(String, nullable=False)
+    key = Column(String, nullable=False)
     last_used = Column(String, nullable=True)
+    
     user_id = Column(Integer, ForeignKey("users.id"))
 
     user = relationship("User", back_populates="secrets")
+    usages = relationship("Usage", back_populates="secret", cascade="all, delete-orphan")
+
+
+class Usage(Base):
+    __tablename__= "usage"
+    
+    id= Column(Integer, primary_key=True)
+    model = Column(String, nullable=False)
+    cost = Column(Float, nullable=False)
+    tokens = Column(Integer, nullable=False)
+    date_time = Column(String, nullable=False)
+    
+    provider = Column(String, ForeignKey("secrets.provider"))
+    user_id = Column(Integer, ForeignKey("users.id"))
+    secrets_id = Column(Integer, ForeignKey("secrets.id"))
+    
+    secret = relationship("Secret", back_populates="usages", cascade="all, delete-orphan")
