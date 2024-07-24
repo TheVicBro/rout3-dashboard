e98e223d4187d01fcbede02795ad27ea523b49a9
Alain Ballen <alainballen@gmail.com>
2024-07-23 23:15:35 -0400

    clean up

--- api-server/services/myapi.py
+++ api-server/services/myapi.py
@@ -1,26 +1,15 @@
-from fastapi import Depends, HTTPException, status, Security
+from fastapi import HTTPException, status, Security
 from fastapi.security import APIKeyHeader
 from sqlalchemy.orm import Session
-from db.database import get_db
-from db.repositories import user_repository as user_repo
 from db.repositories import myapi_repository as myapi_repo
-from services.security import verify_password
-from services import jwt
-from typing_extensions import Annotated
-from models.models import User
-
 import secrets
 
-API_KEY_NAME = "access_token"
+API_KEY_NAME = "api_key"
 api_key_header = APIKeyHeader(name=API_KEY_NAME, auto_error=True)
-
-key = ""
 
 
 def generate_api_key():
-    global key
-    key = secrets.token_hex()
-    return key  # Generates a 64-character hex string
+    return secrets.token_hex()  # Generates a 64-character hex string
 
 
 def get_api_key_from_header(api_key_header: str = Security(api_key_header)):
