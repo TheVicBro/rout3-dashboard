e5799a7fce3cce616eb65227354bd12e0d5e9445
Dinuja Wattage <dinuja.w9@gmail.com>
2024-07-28 11:00:13 -0400

    updated usage table + updated usage schema

--- api-server/schemas/schemas.py
+++ api-server/schemas/schemas.py
@@ -1,9 +1,10 @@
 from pydantic import BaseModel
-from typing import List, Optional
+from typing import List, Dict
+from datetime import datetime
 
 
 class SecretBase(BaseModel):
-    provider: str
+    name: str
     last_used: str
 
 
@@ -12,6 +13,7 @@ class SecretCreate(SecretBase):
     Seperate key from base model
     so it won't be sent from API when reading a secrets
     """
+
     key: str
 
 
@@ -43,16 +45,53 @@ class User(UserBase):
     class Config:
         orm_mode = True
 
-class UsageBase(BaseModel):
-    model: str
-    cost: float
-    token: int
-    provider: str
-    date_time: str
-    
-class Usage(UsageBase):
+
+class MyApiBase(BaseModel):
     id: int
     user_id: int
+
+
+class MyApiCreate(MyApiBase):
+    key: str
+
+
+class UsageBase(BaseModel):
+    user_id: int
     secret_id: int
+    provider: str
+    model: str
+    prompt: str
+    response: str
+    chat_history: List[Dict[str, str]]
+    date_time: datetime
+    input_cost: float
+    prompt_cost: float
+    response_cost: float
+    prompt_tokens: int
+    response_tokens: int
+
+
+class UsageCreate(UsageBase):
+    api_key: str
+
+
+class Usage(UsageBase):
+    id: int
+
+
+class UsageData(BaseModel):
+    data: List[Usage]
+    error: str
+    status: int
+
     class Config:
-        orm_mode: True
+        orm_mode = True
+
+
+class UsageCreateData(BaseModel):
+    data: Usage | Dict
+    error: str
+    status: int
+
+    class Config:
+        orm_mode = True
