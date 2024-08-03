5955a6b35c4a04575d54354244af8dff4fca4a46
laxitshahi <shahilax@my.yorku.ca>
2024-08-02 20:03:51 -0400

    added utils for local dev

--- api-server/app/api/main.py
+++ api-server/app/api/main.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-from app.api.v1 import login, myapi, secrets, user
+from app.api.v1 import login, myapi, secrets, user, utils
 
 api_router = APIRouter()
 
@@ -8,3 +8,4 @@ api_router.include_router(login.router, tags=["login"])
 api_router.include_router(user.router, prefix="/user", tags=["user"])
 api_router.include_router(secrets.router, prefix="/secrets", tags=["secrets"])
 api_router.include_router(myapi.router, prefix="/myapi", tags=["myapi"])
+api_router.include_router(utils.router, prefix="/utils", tags=["utils"])
