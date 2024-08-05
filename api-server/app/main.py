8f1916914d9624c209386bfb87d3b1e2476561e5
laxitshahi <shahilax@my.yorku.ca>
2024-08-05 16:00:11 -0400

    added mock user on startup

--- api-server/app/main.py
+++ api-server/app/main.py
@@ -4,15 +4,32 @@ from starlette.middleware.cors import CORSMiddleware
 
 from app.api.main import api_router
 from app.core.config import settings
-from app.core.database import Base, engine
+from app.core.database import Base, SessionLocal, engine
+from app.models import User
+from app.repositories import user_repo
+from app.schemas import UserCreate
 
 
 def custom_generate_unique_id(route: APIRoute) -> str:
     return f"{route.tags[0]}-{route.name}"
 
 
-# Manual for now
-Base.metadata.create_all(bind=engine)
+def init_db():
+    Base.metadata.create_all(bind=engine)
+
+    db = SessionLocal()
+    try:
+        user = db.query(User).filter(User.username == "a").first()
+        if not user:
+            user_in = UserCreate(username="a", password="a", email="a@gmail.com")
+            user_repo.create_user(db=db, user=user_in, is_admin=False)
+        db.commit()
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        db.rollback()
+    finally:
+        db.close()
+
 
 app = FastAPI(
     title=settings.PROJECT_NAME,
@@ -34,3 +51,9 @@ if settings.BACKEND_CORS_ORIGINS:
     )
 
 app.include_router(api_router, prefix=settings.API_V1_STR)
+
+
+# Bind Tables and add first user to db
+@app.on_event("startup")
+async def startup_event():
+    init_db()
