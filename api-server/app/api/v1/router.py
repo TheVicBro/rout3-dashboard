330c255ee094d479ea05a39c0112527ad965d2d7
Dinuja Wattage <dinuja.w9@gmail.com>
2024-08-08 19:02:41 -0400

    moved litellm calls into services folder

---
+++ api-server/app/api/v1/router.py
@@ -0,0 +1,90 @@
+from typing import List, Dict, Annotated
+import os
+from sqlalchemy.orm import Session
+from fastapi import APIRouter, Header, Depends, HTTPException, status
+# from models.schemas.schemas import CompletionResponse, RouteRequest
+from app.repositories.myapi_repo import get_user_id_by_myapi
+from app.api.deps import get_db
+from app.services import myapi, router as litellm_router
+from app.schemas import RouteRequest
+
+router = APIRouter()
+
+@router.post('/completion')  # response_model=CompletionResponse)
+def chat_completion(
+    data: RouteRequest, 
+    myapi_key: str = Header(None), db: Session = Depends(get_db)):
+    # grab the API key from the auth header of the request
+    # if authorization is None:
+    #     raise HTTPException(
+    #         status_code=status.HTTP_403_FORBIDDEN, 
+    #         detail="API key not provided. Can not access router."
+    #     )
+    # else:
+    api_key = myapi_key
+    # verify API key -> will need to wait on the merge
+    verified = myapi.verify_api_key(db=db, api_key_string=api_key)
+    if verified:
+        # make GET request to the endpoint 'api/v1/configuration' and grab the user settings
+        # and to make that call, we'd need to query by user_id. How would we grab the user_id?
+        # We could just grab the id based on the Rout3 API Key passed in
+
+        # 1) FETCH user_id USING API KEY -> GET 'api/v1/myapi/userid'
+        user_id = get_user_id_by_myapi(db=db, myapi_key=api_key)
+        # 2) get user_settings using user_id -> from config
+        user_settings = [
+            {
+                "model_name": "router",  # model alias for routing/load balancing
+                "litellm_params": {
+                    "model": "command",
+                    "api_key": os.getenv("COHERE_API_KEY"),
+                    "timeout": 1,
+                    "max_tokens": 200
+                }
+            },
+            {
+                "model_name": "router",
+                "litellm_params": {
+                    "model": "command-nightly",
+                    "api_key": os.getenv("COHERE_API_KEY"),
+                    "timeout": 1,
+                    "max_tokens": 200
+                }
+            },
+            {
+                "model_name": "router", 
+                "litellm_params": { 
+                    "model": "command-r", 
+                    "api_key": os.getenv("COHERE_API_KEY"),
+                    "timeout": 1,
+                    "max_tokens": 200
+                }
+            },
+            {
+                "model_name": "router",
+                "litellm_params": {
+                    "model": "command-r-plus",
+                    "api_key": os.getenv("COHERE_API_KEY"),
+                    "timeout": 1,
+                    "max_tokens": 200
+                }
+            }
+        ]
+        analytics_data = litellm_router.get_completion(user_settings, data, user_id)
+        print(analytics_data)
+        # make any API calls to grab data like: user_id, 
+        
+        # make a POST request to 'api/v1/analytics/record' to send the data in 'body' and pass in the API key into the header for auth
+        
+        # if data was successfully sent to 'record' endpoint
+            # return the CompletionResponse
+
+        # if request to 'record' endpoint errored out
+            # raise the error
+    else:
+        # raise invalid API key exception with error code 401 and message "Invalid API key. Ensure the correct API key and requesting 
+        # organization are being used."
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid API key. Ensure the correct API key and requesting organization are being used.", 
+        )
