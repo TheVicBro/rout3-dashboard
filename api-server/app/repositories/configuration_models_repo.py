04be742160541114fcdf53d60adeedc086367277
Alain Ballen <alainballen@gmail.com>
2024-08-09 02:59:40 -0400

    crud operations

---
+++ api-server/app/repositories/configuration_models_repo.py
@@ -0,0 +1,62 @@
+from sqlalchemy.orm import Session
+
+from app.models import Config_Models
+
+
+# create configuration
+def create_config_models(db: Session, models: str, max_tokens: int, temperature: float):
+    db_config_model = Config_Models(
+        models=models, max_tokens=max_tokens, temperature=temperature
+    )
+    db.add(db_config_model)
+    db.commit()
+    db.refresh(db_config_model)
+    return db_config_model
+
+
+# update configuration
+def update_config_models(
+    db: Session, config_model_id: int, models: str, max_tokens: int, temperature: float
+):
+    db_config_model = (
+        db.query(Config_Models).filter(Config_Models.id == config_model_id).first()
+    )
+
+    # update only given parameters
+    if models is not None:
+        db_config_model.models = models
+    if max_tokens is not None:
+        db_config_model.max_tokens = max_tokens
+    if temperature is not None:
+        db_config_model.temperature = temperature
+
+    db.commit()
+    db.refresh(db_config_model)
+
+
+# get configuration models by id
+def get_configuration_models_by_id(db: Session, config_models_id: int):
+    data = db.query(Config_Models).filter(Config_Models.id == config_models_id).first()
+    return data
+
+
+# get all configuration models by configuration id
+def get_configuration_models_by_id(
+    db: Session, config_id: int, skip: int = 0, limit: int = 10
+):
+    data = (
+        db.query(Config_Models)
+        .filter(Config_Models.config_id == config_id)
+        .offset(skip)
+        .limit(limit)
+        .all()
+    )
+    return data
+
+
+# delete model
+def delete_config_model(db: Session, config_model_id: int):
+    # It will be successful no matter what
+    data = db.query(Config_Models).where(Config_Models.id == config_model_id).delete()
+    db.commit()
+    return data
--- api-server/app/repositories/configurations_repo.py
+++ api-server/app/repositories/configurations_repo.py
@@ -1,14 +1,45 @@
-from sqlalchemy import and_
 from sqlalchemy.orm import Session
 
 from app.models import Config
 
+
 # create configuration
+def create_config(db: Session, route_type: str, timeout: int):
+    db_config = Config(route_type=route_type, timeout=timeout)
+    db.add(db_config)
+    db.commit()
+    db.refresh(db_config)
+    return db_config
+
 
 # update configuration
+def update_configuration(db: Session, config_id: int, route_type: str, timeout: int):
+    db_config = db.query(Config).filter(Config.id == config_id).first()
+
+    # update only given parameters
+    if route_type is not None:
+        db_config.route_type = route_type
+    if timeout is not None:
+        db_config.timeout = timeout
+
+    db.commit()
+    db.refresh(db_config)
 
 
 # get configuration by id
+def get_configuration_by_id(db: Session, config_id: int):
+    data = db.query(Config).filter(Config.config_id == config_id).first()
+    return data
 
 
-# get all configuration for user
+# get all configurations for a user
+def get_configuration_by_user_id(
+    db: Session, user_id: int, skip: int = 0, limit: int = 10
+):
+    return (
+        db.query(Config)
+        .filter(Config.user_id == user_id)
+        .offset(skip)
+        .limit(limit)
+        .all()
+    )
