84c5eaf0f7e0bf03d2c9403b2e0a7ba956a4278b
Dinuja Wattage <dinuja.w9@gmail.com>
2024-08-09 10:38:00 -0400

    clean up

--- api-server/app/api/v1/router.py
+++ api-server/app/api/v1/router.py
@@ -13,7 +13,9 @@ router = APIRouter()
 @router.post('/completion')  # response_model=CompletionResponse)
 def chat_completion(
     data: RouteRequest, 
-    myapi_key: str = Header(None), db: Session = Depends(get_db)):
+    myapi_key: str = Header(None), 
+    db: Session = Depends(get_db)
+    ):
     # grab the API key from the auth header of the request
     # if authorization is None:
     #     raise HTTPException(
@@ -21,10 +23,14 @@ def chat_completion(
     #         detail="API key not provided. Can not access router."
     #     )
     # else:
+    # api_key = "9624737a2087331b78c0fea213a926f1ebbb78d5a36e11deac674c82451ab577"
+    # api_key = "gAAAAABmth5XFAMH6QEd-w4EIncdcuQYoa35IJWj7MSqTxUAdW-Zj3RJw6WYg8fdMKtpbqU5WhbRTBXV55RO3iw-WmXfsK6zmPIB-wA8IUsKqSk571dacM92zNILr9-NP2R6hH7LrhBLvHc43nk5cNAgJFxNdricZEOUxj-f2xzzZtiXlUDWE44="
+    # api_key = "gAAAAABmtVSSItJmiSV7GZx2LlCrM0CVRRwN6z2lZ5Kxr5aULrXEoSFYkC7KRYT17AA339Kxdofi-jWj9QMafrxZJVAOkTVs_B5WZmx_01m5r6REFHti-L0CB3L01Gzko3WuT3n4xQeebLXR4aL124x1UZ_8fTSN1WmOFLWM8PkZH5_Hx2pJmoU="
     api_key = myapi_key
+
     # verify API key -> will need to wait on the merge
-    verified = myapi.verify_api_key(db=db, api_key_string=api_key)
-    if verified:
+    verified_rout3_key = myapi.verify_api_key(db=db, api_key_string=api_key)
+    if verified_rout3_key:
         # make GET request to the endpoint 'api/v1/configuration' and grab the user settings
         # and to make that call, we'd need to query by user_id. How would we grab the user_id?
         # We could just grab the id based on the Rout3 API Key passed in
@@ -37,9 +43,10 @@ def chat_completion(
                 "model_name": "router",  # model alias for routing/load balancing
                 "litellm_params": {
                     "model": "command",
-                    "api_key": os.getenv("COHERE_API_KEY"),
+                    "api_key": os.getenv("COHERE_API_KEY"), # PUT SECRET HERE
                     "timeout": 1,
-                    "max_tokens": 200
+                    "max_tokens": 200,
+                    "temperature": 0.98
                 }
             },
             {
@@ -48,7 +55,9 @@ def chat_completion(
                     "model": "command-nightly",
                     "api_key": os.getenv("COHERE_API_KEY"),
                     "timeout": 1,
-                    "max_tokens": 200
+                    "max_tokens": 200,
+                    "temperature": 0.98
+                    
                 }
             },
             {
@@ -57,7 +66,8 @@ def chat_completion(
                     "model": "command-r", 
                     "api_key": os.getenv("COHERE_API_KEY"),
                     "timeout": 1,
-                    "max_tokens": 200
+                    "max_tokens": 200,
+                    "temperature": 0.98
                 }
             },
             {
@@ -66,7 +76,8 @@ def chat_completion(
                     "model": "command-r-plus",
                     "api_key": os.getenv("COHERE_API_KEY"),
                     "timeout": 1,
-                    "max_tokens": 200
+                    "max_tokens": 200,
+                    "temperature": 0.98
                 }
             }
         ]
@@ -75,7 +86,7 @@ def chat_completion(
         # make any API calls to grab data like: user_id, 
         
         # make a POST request to 'api/v1/analytics/record' to send the data in 'body' and pass in the API key into the header for auth
-        
+                
         # if data was successfully sent to 'record' endpoint
             # return the CompletionResponse
 
@@ -85,6 +96,6 @@ def chat_completion(
         # raise invalid API key exception with error code 401 and message "Invalid API key. Ensure the correct API key and requesting 
         # organization are being used."
         raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Invalid API key. Ensure the correct API key and requesting organization are being used.", 
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Could not validate credentials",
         )
--- api-server/app/services/myapi.py
+++ api-server/app/services/myapi.py
@@ -22,9 +22,5 @@ def get_api_key_from_header(api_key_header: str = Security(api_key_header)):
 def verify_api_key(db: Session, api_key_string):
     api_key = myapi_repo.get_myapi(db, api_key_string)
     if api_key is None:
-        raise HTTPException(
-            status_code=status.HTTP_403_FORBIDDEN,
-            detail="Could not validate credentials",
-        )
-
-    return api_key
+        return False
+    return True
