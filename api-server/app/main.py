de90bb75fb0415a0733a8ad9601ebfed9cb2dbda
Victor Chung <dragonslayer3211@gmail.com>
2024-08-27 12:29:57 -0400

    test 2

--- api-server/app/main.py
+++ api-server/app/main.py
@@ -37,3 +37,8 @@ app.include_router(api_router, prefix=settings.API_V1_STR)
 @app.on_event("startup")
 async def startup_event():
     init_db()
+
+
+@app.get('/')
+def hello_world():
+    return "Hello,World"
