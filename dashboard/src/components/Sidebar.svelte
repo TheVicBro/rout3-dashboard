f9f7e8492aa1cc4001f4033de8ac11c36303bc85
Victor Chung <dragonslayer3211@gmail.com>
2024-08-27 14:19:19 -0400

    Logout fix

--- dashboard/src/components/Sidebar.svelte
+++ dashboard/src/components/Sidebar.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import { Link, useLocation } from 'svelte-routing';
+  import { navigate, Link, useLocation } from 'svelte-routing';
   import { isAuthenticated } from '../stores/auth';
   import { Button } from "$lib/components/ui/button/index.js";
   import { MessageSquare, Bot, FileSliders, KeyRound, BarChart3, Wallet, UserRound, Settings, LogOut } from 'lucide-svelte';
@@ -20,12 +20,12 @@
   const bottomMenuItems = [
     { path: '/account', label: 'Account', icon: UserRound },
     { path: '/settings', label: 'Settings', icon: Settings },
-    { path: '/login', label: 'Logout', icon: LogOut, onClick: logout },
   ];
 
   function logout() {
     localStorage.clear();
     isAuthenticated.set(false);
+    navigate('/login', { replace: true });
   }
 </script>
 
@@ -51,20 +51,27 @@
       {/each}
     </div>
     <div class="pb-8 pt-8 border-t-2 dark:border-black">
-      {#each bottomMenuItems as { path, label, icon, onClick }}
+      {#each bottomMenuItems as { path, label, icon }}
         <Link to={path} class="w-full">
           <Button
             class={`menu-item w-full h-12 p-2 rounded-lg flex items-center justify-start space-x-2 transition bg-white dark:bg-slate-900 ${
               currentPath === path ? 'bg-blue-100 dark:bg-blue-900 hover:bg-blue-200 hover:dark:bg-blue-800 text-blue-800 dark:text-blue-100' : 'text-gray-500 dark:text-slate-200 hover:bg-gray-200 hover:dark:bg-slate-800 hover:text-gray-700 hover:dark:text-white'
             }`}
             type="button"
-            on:click={onClick}
           >
             <svelte:component this={icon} />
             <span class="font-semibold">{label}</span>
           </Button>
         </Link>
       {/each}
+      <Button
+        class={`menu-item w-full h-12 p-2 rounded-lg flex items-center justify-start space-x-2 transition bg-white dark:bg-slate-900 text-gray-500 dark:text-slate-200 hover:bg-gray-200 hover:dark:bg-slate-800 hover:text-gray-700 hover:dark:text-white`}
+        type="button"
+        on:click={logout}
+      >
+        <svelte:component this={LogOut} />
+        <span class="font-semibold">Logout</span>
+      </Button>
     </div>
   </div>
 </div>
