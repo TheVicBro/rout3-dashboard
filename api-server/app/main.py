395cd27e3cc301e7d1ff2f511e0d70039c4d203d
Victor Chung <dragonslayer3211@gmail.com>
2025-12-06 11:19:28 -0500

    dev: temporarily allow all origins

--- api-server/app/main.py
+++ api-server/app/main.py
@@ -35,10 +35,10 @@ if settings.BACKEND_CORS_ORIGINS:
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=allowed_origins,
+    allow_origins=["*"],  # Temporarily allow all origins to debug
     allow_credentials=True,
     allow_methods=["*"],
-    allow_headers=["*"],
+    allow_headers=["*", "myapi-key", "content-type"], # Explicitly allow myapi-key
     expose_headers=["*"],
     max_age=3600,
 )
